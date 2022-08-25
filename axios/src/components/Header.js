892eaf5da813eb809f035d53018693db61726bbd
Tyler Morgan <clickjaw@outlook.com>
2022-08-25 09:24:05 -0500

    done?

--- axios/src/components/Header.js
+++ axios/src/components/Header.js
@@ -2,24 +2,11 @@ import React, { Component } from 'react'
 
 import { Outlet } from 'react-router-dom'
 
-import { LinkContainer } from 'react-router-bootstrap'
-
-import { NavLink, Nav } from 'react-bootstrap'
-
 export default class Header extends Component {
   render() {
     return (
       <>
-        <Nav className='navMenu' style={{paddingRight: '2rem', marginTop: "-60px", display: 'flex', justifyContent:'center', fontFamily:"Carter One",}}>
-            <LinkContainer to='/'>
-                <NavLink>Home</NavLink>
-            </LinkContainer>
-            <LinkContainer to='about'>
-                <NavLink>About</NavLink>
-            </LinkContainer>
-        </Nav>
         <h1 className = "header">World Wide City Search</h1>
-        {/* <h3>powered by <img style={{width: '100px'}} alt='LocationIQ Logo' src={locationIQ}/></h3> */}
         <Outlet/>
         
       </>
--- axios/src/index.js
+++ axios/src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import About from './routes/about';
 import Header from './components/Header';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -12,7 +11,6 @@ root.render(
   <BrowserRouter>
     <Routes>
       <Route path='/' element={<App/>}/>
-      <Route path='about' element={<About/>}/>
       <Route path='*' element={<div className='application-container'><Header/><hr/><h1>404 Not Found</h1></div>}/>
     </Routes>
   </BrowserRouter>
